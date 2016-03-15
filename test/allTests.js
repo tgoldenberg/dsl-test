e032676871587239d1318c19a738d00e4c263695
jp <jplikesbikes@gmail.com>
2016-03-14 23:21:56 -0400

    document allTests.js

--- test/allTests.js
+++ test/allTests.js
@@ -1,7 +1,9 @@
 'use strict';
 
+// This is webpacks `require.context` plugin that during compile
+// gets a list of the files in the directory matching the regex recursively
 /* global require */
 const testRequire = require.context('./', true, /\.spec\.js$/);
 
-// Require everything
+// Require everything and put it all into scope
 testRequire.keys().map(testRequire);
