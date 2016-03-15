77c09ffceea796da73d59d487b8dfa1d9020fac3
jp <jplikesbikes@gmail.com>
2016-03-14 22:38:05 -0400

    add an index entry point, and a compile script to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,10 @@ module.exports = {
 			{ test: /\.(eot|woff|woff2|ttf|svg|png|jpg)(\?.*)?$/, loader: 'file-loader' },
 			{ test: /\.css$/, loader: 'style!css!postcss' },
 			{ test: /\.less$/, loader: 'style!css!postcss!less' },
-			{ test: /\.jade$/, loader: 'html?removeRedundantAttributes=false!jade-html' },
-			{ test: /\.html$/, loader: 'html?removeRedundantAttributes=false' }
+			{ test: /\.tpl\.jade$/, loader: 'html?removeRedundantAttributes=false!jade-html' },
+			{ test: /\.tpl\.html$/, loader: 'html?removeRedundantAttributes=false' },
+			{ test: /[^\.][^t][^p][^l]\.jade$/, loader: 'file?name=[name].html!jade-html' },
+			{ test: /[^\.][^t][^p][^l]\.html$/, loader: 'file?name=[name].[ext]' }
 		]
 	},
 	postcss: function(){
@@ -18,6 +20,8 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.SourceMapDevToolPlugin({
+			// exclude the index entry point
+			exclude: /.*index.*$/,
 			columns: false,
 			filename: '[file].map[query]',
 			lineToLine: false,
@@ -25,8 +29,8 @@ module.exports = {
 		})
 	],
 	debug: true,
-	entry: { app: './app.js' },
-	output: { filename: '[name].bundle.js', path: '/' },
+	entry: { app: './app.js', index: './index.html' },
+	output: { filename: '[name].bundle.js', path: './bin/' },
 	context: process.cwd() + '/src',
 	devServer: {
 		publicPath: '/',
